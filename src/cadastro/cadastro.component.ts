a55af5e99637622ab9b29edfda9cbbeb7c83995a
EvokSix <70288620+EvokSix@users.noreply.github.com>
2021-10-21 20:56:35 -0300

    adicionado o validar campo service

--- src/cadastro/cadastro.component.ts
+++ src/cadastro/cadastro.component.ts
@@ -1,3 +1,4 @@
+import { ValidarCamposService } from './../shared/componentes/campos/validar-campo.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -7,8 +8,9 @@ import { Router } from '@angular/router';
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css'],
 })
+
 export class CadastroComponent implements OnInit {
-  constructor(private router: Router, private fb: FormBuilder) {}
+  constructor(private router: Router, private fb: FormBuilder,public validacao: ValidarCamposService) {}
 
   password!: string;
   email!: string;
@@ -23,13 +25,17 @@ export class CadastroComponent implements OnInit {
     });
   }
 
+  get f(){
+    return this.cadastro.controls;
+  }
+
   cadastrar(): void {
     if (this.cadastro.valid) {
       //this.router.navigate(["app-root"]);
       alert("Cadastro efetuado com sucesso");
       this.router.navigate([""]);
     } else {
-      alert('Não Entrou');
+      alert('Erro ao cadastrar');
     }
   }
 }
